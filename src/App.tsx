1b9db4fb79b5dd89d2eed2bc7dd16204e22db24b
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2025-06-10 21:41:07 -0300

    addGoggle

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,38 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      {/* Google Tag Manager */}
+      <script dangerouslySetInnerHTML={{ __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','YOUR_GTM_ID');
+      `}} />
+      {/* Facebook Pixel Code */}
+      <script dangerouslySetInnerHTML={{ __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', 'YOUR_FACEBOOK_PIXEL_ID');
+        fbq('track', 'PageView');
+      `}} />
+      {/* Google Pixel Code */}
+      <script dangerouslySetInnerHTML={{ __html: `
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_GOOGLE_PIXEL_ID"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'YOUR_GOOGLE_PIXEL_ID');
+        </script>
+      `}} />
       <Toaster />
       <Sonner />
       <BrowserRouter>
